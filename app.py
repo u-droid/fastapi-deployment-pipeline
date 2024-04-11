5a809febf8b34a77128cda59856dbc82f5bb8af7
u-droid <u.tyagi2013@gmail.com>
2024-04-11 21:01:02 +0530

    fixed pylint errors

--- app.py
+++ app.py
@@ -4,14 +4,11 @@ Code to test Continuous Integration using Github Actions
 import time
 
 def app():
-    try:
-        """Test App"""
-        print("App is running")
-        time.sleep(2)
-        print("App execution is completed")
-        return True
-    except Exception as e:
-        return False
+    """Test App"""
+    print("App is running")
+    time.sleep(2)
+    print("App execution is completed")
+    return True
 
 if __name__=="__main__":
-    assert app()==True
+    assert app() is True
